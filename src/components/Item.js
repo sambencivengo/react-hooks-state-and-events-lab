a24b23e183018a30f14ba3052e2aaadd2fa88fd0
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-18 21:07:27 -0400

    commit tree 8c611a0946e399cb30766e0af01a7ea49ce77e0a

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+	const [addToCart, setAddToCart] = useState(false);
+
+	// use another ternary to add to cart
+
+	const handleClick = (e) => {
+		setAddToCart(!addToCart);
+		addToCart
+			? (e.target.textContent = 'Add to Cart')
+			: (e.target.textContent = 'Remove from cart');
+	};
+
+	return (
+		<li className={addToCart ? 'in-cart' : ''}>
+			<span>{name}</span>
+			<span className="category">{category}</span>
+			<button onClick={handleClick} className="add">
+				Add to Cart
+			</button>
+		</li>
+	);
 }
 
 export default Item;
